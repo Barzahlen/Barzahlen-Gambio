6159f5130cf38a04c62335cc2a3b59aeb68d446d
Alexander Diebler <alexander.diebler@barzahlen.de>
2014-11-11 10:54:39 +0100

    Version 1.2.0
    - integrated Barzahlen PHP SDK v1.1.7 (Payment & Cancel)
    - automatic payment slip cancellation for cancelled orders
    - callback returns 200 (OK) only after successful database update
    - improved payment selection

--- src/admin/includes/modules/barzahlen_version_check.php
+++ src/admin/includes/modules/barzahlen_version_check.php
@@ -2,74 +2,39 @@
 /**
  * Barzahlen Payment Module (Gambio)
  *
- * NOTICE OF LICENSE
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- * @copyright   Copyright (c) 2013 Zerebro Internet GmbH (http://www.barzahlen.de)
+ * @copyright   Copyright (c) 2014 Cash Payment Solutions GmbH (https://www.barzahlen.de)
  * @author      Mathias Hertlein
+ * @author      Alexander Diebler
  * @license     http://opensource.org/licenses/GPL-2.0  GNU General Public License, version 2 (GPL-2.0)
  */
 
 defined('_VALID_XTC') or die('Direct Access to this location is not allowed.');
 
-require_once("barzahlen/BarzahlenHashCreate.php");
 require_once("barzahlen/BarzahlenHttpClient.php");
 require_once("barzahlen/BarzahlenPluginCheckRequest.php");
 require_once("barzahlen/BarzahlenVersionCheck.php");
-require_once("barzahlen/BarzahlenConfigRepository.php");
 
 require_once(DIR_FS_LANGUAGES . $_SESSION['language'] . "/modules/payment/barzahlen.php");
-require_once(DIR_FS_CATALOG . 'release_info.php');
+include(DIR_FS_CATALOG . 'release_info.php');
 
 $httpClient = new BarzahlenHttpClient();
 $barzahlenVersionCheckRequest = new BarzahlenPluginCheckRequest($httpClient);
-$barzahlenRepository = new BarzahlenConfigRepository();
-
-$barzahlenVersionCheck = new BarzahlenVersionCheck($barzahlenVersionCheckRequest, $barzahlenRepository);
+$barzahlenVersionCheck = new BarzahlenVersionCheck($barzahlenVersionCheckRequest);
 
 try {
-    if (MODULE_PAYMENT_BARZAHLEN_STATUS == "True" && !$barzahlenVersionCheck->isCheckedInLastWeek(time())) {
-        $barzahlenVersionCheck->check(MODULE_PAYMENT_BARZAHLEN_SHOPID, MODULE_PAYMENT_BARZAHLEN_PAYMENTKEY, $gx_version);
+    if (MODULE_PAYMENT_BARZAHLEN_STATUS == "True" && !$barzahlenVersionCheck->isCheckedInLastWeek()) {
+        $barzahlenVersionCheck->check(MODULE_PAYMENT_BARZAHLEN_SHOPID, $gx_version);
         $displayUpdateAvailableMessage = $barzahlenVersionCheck->isNewVersionAvailable();
-        $barzahlenNewestVersion = $barzahlenVersionCheck->getNewestVersion();
     } else {
         $displayUpdateAvailableMessage = false;
     }
 } catch (Exception $e) {
+    error_log('barzahlen/versioncheck: ' . $e, 3, DIR_FS_CATALOG . 'logfiles/barzahlen.log');
     $displayUpdateAvailableMessage = false;
 }
-?>
 
-<?php
 if ($displayUpdateAvailableMessage) {
-?>
-
-<table style="border: 1px solid; border-color: #ff0000;"
-       bgcolor="#FDAC00" border="0" width="100%" cellspacing="0"
-       cellpadding="0">
-    <tr>
-        <td><div class="main">
-            <table width="100%" border="0">
-                <tr>
-                    <td class="main"><?php echo sprintf(MODULE_PAYMENT_BARZAHLEN_NEW_VERSION, $barzahlenNewestVersion) ?></td>
-                </tr>
-            </table>
-            </div>
-        </td>
-    </tr>
-</table>
-
-<?php
+    echo '<table cellspacing="0" cellpadding="0" style="width: 100%; background-color: #fdac00; border: 1px solid #ff0000; font-family: Verdana,Arial,sans-serif; font-size: 12px;"><tr><td><div class="main"><table style="width: 100%; border:0"><tr><td class="main">';
+    echo sprintf(MODULE_PAYMENT_BARZAHLEN_NEW_VERSION, $barzahlenVersionCheck->getNewestVersion(), $barzahlenVersionCheck->getNewestVersionUrl());
+    echo '</td></tr></table></div></td></tr></table>';
 }
